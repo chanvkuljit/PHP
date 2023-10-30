028cb414275923e86224d670daad7159bc1973a9
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-10-30 15:09:15 +0530

    Create 2.php

---
+++ Unit_1/2.php
@@ -0,0 +1,12 @@
+<?php
+// Sample array of numbers
+$numbers = array(5, 2, 8, 1, 9, 4, 6);
+
+// Find the maximum and minimum numbers
+$maxNumber = max($numbers);
+$minNumber = min($numbers);
+
+// Print the results
+echo "The maximum number is: " . $maxNumber . "\n";
+echo "The minimum number is: " . $minNumber . "\n";
+?>
