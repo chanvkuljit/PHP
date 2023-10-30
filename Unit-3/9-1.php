42300c4bf0809be6a6f4af67165d2c3473259200
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-10-30 17:05:31 +0530

    Create 9-1.php

---
+++ Unit-3/9-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="store_data.php">
+  Username:<br>
+  <input type="text" name="username"><br><br>
+  Email:<br>
+  <input type="text" name="email"><br><br>
+  Password:<br>
+  <input type="password" name="password"><br><br>
+  <input type="submit" name="submit" value="Register">
+</form>
+
+</body>
+</html>
