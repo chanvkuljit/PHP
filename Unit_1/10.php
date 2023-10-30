775ee76a391da71362508c17fe957aaed45df145
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-10-30 16:07:47 +0530

    Create 10.php

---
+++ Unit_1/10.php
@@ -0,0 +1,12 @@
+<?php
+// Define two sample arrays
+$array1 = array("apple", "banana", "orange");
+$array2 = array("carrot", "broccoli", "peas");
+
+// Merge the two arrays
+$mergedArray = array_merge($array1, $array2);
+
+// Print the merged array
+echo "Merged array: \n";
+print_r($mergedArray);
+?>
