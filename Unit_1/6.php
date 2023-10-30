1fbb59e0b69138c9b181a90321bca7d8e7de1066
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-10-30 15:17:25 +0530

    Create 6.php

---
+++ Unit_1/6.php
@@ -0,0 +1,15 @@
+<?php
+echo "Printing numbers from 15 to 20 using while loop:\n";
+$i = 15;
+while ($i <= 20) {
+    echo $i . "\n";
+    $i++;
+}
+
+echo "Printing numbers from 15 to 20 using do-while loop:\n";
+$i = 15;
+do {
+    echo $i . "\n";
+    $i++;
+} while ($i <= 20);
+?>
