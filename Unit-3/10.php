73023a4d388bacf7accf2e4eb3fb0a486035d6d9
Chanv Kuldeep <93032744+chanvkuljit@users.noreply.github.com>
2023-10-30 17:06:22 +0530

    Create 10.php

---
+++ Unit-3/10.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Prepare and bind
+$stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $username, $email, $password);
+
+// Set parameters and execute
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$stmt->execute();
+
+echo "New records created successfully";
+
+$stmt->close();
+$conn->close();
+?>
